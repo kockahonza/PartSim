cd3d66b4d8d053e38c0f08a768e70a5fa01790d3
Jan Kočka <kockahonza@gmail.com>
2022-10-06 20:05:36 +0200

    added save_every setting to save memory

--- apps/LennardJonesFast.cpp
+++ apps/LennardJonesFast.cpp
@@ -23,6 +23,7 @@ struct config {
     double T;
     int max_iter;
 
+    int save_every;
     std::string output_filename;
 };
 
@@ -42,6 +43,7 @@ int main() {
         config_file.lookup("LennardJonesFast.dt"),
         config_file.lookup("LennardJonesFast.T"),
         config_file.lookup("LennardJonesFast.max_iter"),
+        config_file.lookup("LennardJonesFast.save_every"),
         config_file.lookup("LennardJonesFast.output_filename")
     };
 
@@ -60,19 +62,22 @@ int main() {
     File h5_file{cfg.output_filename, File::Overwrite};
 
     // Setup storage for the data to be collected and saved
-    vector<vector<Eigen::Vector3d>> positions(cfg.max_iter, vector<Eigen::Vector3d>(particle_count));
-    vector<vector<Eigen::Vector3d>> velocities(cfg.max_iter, vector<Eigen::Vector3d>(particle_count));
-    vector<vector<Eigen::Vector3d>> forces(cfg.max_iter, vector<Eigen::Vector3d>(particle_count));
+    vector<vector<Eigen::Vector3d>> positions(cfg.max_iter / cfg.save_every, vector<Eigen::Vector3d>(particle_count));
+    vector<vector<Eigen::Vector3d>> velocities(cfg.max_iter / cfg.save_every, vector<Eigen::Vector3d>(particle_count));
+    vector<vector<Eigen::Vector3d>> forces(cfg.max_iter / cfg.save_every, vector<Eigen::Vector3d>(particle_count));
 
     // Run the simulation
-    ps.run(cfg.dt, cfg.T, cfg.max_iter, [&positions, &velocities, &forces](const PartSim& ps, int i) {
-            cout << i << endl;
-            const vector<Particle>& particles{ps.get_particles()};
-            #pragma omp for default(none) shared(positions, velocities, forces)
-            for (size_t j = 0; j < particles.size(); j++) {
-                positions[i][j] = particles[j].get_position();
-                velocities[i][j] = particles[j].get_velocity();
-                forces[i][j] = particles[j].get_force();
+    ps.run(cfg.dt, cfg.T, cfg.max_iter, [&cfg, &positions, &velocities, &forces](const PartSim& ps, int i) {
+            if (i % cfg.save_every == 0) {
+                cout << i << endl;
+                int k{i / cfg.save_every};
+                const vector<Particle>& particles{ps.get_particles()};
+                #pragma omp for default(none) shared(positions, velocities, forces)
+                for (size_t j = 0; j < particles.size(); j++) {
+                    positions[k][j] = particles[j].get_position();
+                    velocities[k][j] = particles[j].get_velocity();
+                    forces[k][j] = particles[j].get_force();
+                }
             }
 
             return true;
@@ -83,6 +88,23 @@ int main() {
     h5_file.createDataSet("velocities", velocities);
     h5_file.createDataSet("forces", forces);
 
+    // Also collect the final state
+    vector<Eigen::Vector3d> final_positions(particle_count);
+    vector<Eigen::Vector3d> final_velocities(particle_count);
+    vector<Eigen::Vector3d> final_forces(particle_count);
+
+    const vector<Particle>& final_particles{ps.get_particles()};
+    cout << particle_count << "bb" << particles.size();
+    #pragma omp for default(none) shared(positions, velocities, forces)
+    for (size_t j = 0; j < particles.size(); j++) {
+        final_positions[j] = final_particles[j].get_position();
+        final_velocities[j] = final_particles[j].get_velocity();
+        final_forces[j] = final_particles[j].get_force();
+    }
+    h5_file.createDataSet("final_positions", final_positions);
+    h5_file.createDataSet("final_velocities", final_velocities);
+    h5_file.createDataSet("final_forces", final_forces);
+
     h5_file.createAttribute("N", cfg.N);
     h5_file.createAttribute("mass", cfg.mass);
     h5_file.createAttribute("epsilon", cfg.epsilon);
@@ -90,6 +112,7 @@ int main() {
     h5_file.createAttribute("initial_spacing", cfg.initial_spacing);
     h5_file.createAttribute("dt", cfg.dt);
     h5_file.createAttribute("T", cfg.T);
+    h5_file.createAttribute("save_every", cfg.save_every);
     h5_file.createAttribute("max_iter", cfg.max_iter);
 
     return 0;
